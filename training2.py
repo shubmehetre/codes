30a6accc4b7862155f9c1820add0be86f07c0e9f
Shubham A Mehetre <shub.mehetre@gmail.com>
2020-04-12 16:38:36 +0530

    doing some practise

--- training2.py
+++ training2.py
@@ -3,11 +3,3 @@ name = 'doom bringer'
 print(name[:3].capitalize())
 
 print('some change')
-# print('some another change')
-
-print('testing diff to compare in git bash')
-<<<<<<< HEAD
-# print('one more')
-=======
-print('changed from github to test git pull command')
->>>>>>> f3f820d5bf1f942983347cee3796eb3d63e7876c
