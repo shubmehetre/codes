6a13094567957440e1b48fdf9da89514bfadfdc6
subz3ro <shub.mehetre@gmail.com>
2021-06-15 14:43:44 +0530

    routine

---
+++ basics/strings.py
@@ -0,0 +1,49 @@
+# simple printing of a string
+str1 = "my first string"
+print(str1)
+
+# indexing in strings
+str2 = 'my second string'
+print(f'0th elemet = {str2[0]}')
+
+# splicing in strings
+print(f'splicing 1 : {str1[1:6:2]}')
+print(f'splicing 2 : {str1[1::2]}')
+
+# 
+print(str1.__len__())
+
+# some string functions 
+print(f'index function : {str2.index(" ")}')
+print(f'title function : {str1.title()}')
+print(f'strip + title function : {str1.strip().title()}')
+print(f'isspace funciton  : {str1.isspace()}')
+
+############################################################################################################################################
+
+# Using variables in strings
+fname = "sam"
+lname = "harris"
+
+# full_name = fname + lname
+full_name = f'{fname} {lname}'
+
+print(full_name.title())
+
+############################################################################################################################################
+
+# user_name = input("enter you name : ")
+# print(f'Hello there, {user_name.title()}')
+############################################################################################################################################
+
+# using some basic string formatting
+author_name = "Robert Shuller"
+quote = "Tough times never last, only Tough people do!"
+print(f'"{quote}"\n\t\t\t --{author_name}')
+############################################################################################################################################
+
+# more stripping 
+another_name = "\tshubham mehetre      "
+print(another_name)
+print(f'now stripping spaces : {another_name.strip()}')
+############################################################################################################################################
---
+++ basics/variables.py
@@ -0,0 +1,13 @@
+_var1 = "apple"
+var2 = 10
+var3 = 'asd'
+var4 = "asd" " zxc"
+
+print(f'var1 = {_var1}')
+print(f'var2 = {var2}')
+print(f'var3 = {var3}')
+print(f'var4 = {var4}')
+print()
+print( f'var2 + 2 = {var2 +2}' )
+
+
