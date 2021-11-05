4881a8911049a7426bb6626f7cbcb35267b50f67
shubmehetre <ghostfr3k@protonmail.com>
2021-11-05 21:40:01 +0530

    auto push

--- java/java-normal/src/DavidBanas/Operators.java
+++ java/java-normal/src/DavidBanas/Operators.java
@@ -1,24 +1,17 @@
 package DavidBanas;
-
-
 import java.util.Scanner;
 
 public class Operators {
 
-    void testPro(){
-        System.out.println("from protected function in Operators class");
-    }
-
     public static void main(String[] args) {
         int i = 5;
-        int j =  10;
         boolean bol = true;
 
-
+        System.out.println("Hello, World");
 
         Scanner sc= new Scanner(System.in);
         int userInput = sc.nextInt();
 
-
     }
 }
+
