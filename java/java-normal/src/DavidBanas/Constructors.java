6337f876b62227e0fdc0fd430331a23743f1a7e4
shubmehetre <ghostfr3k@protonmail.com>
2022-01-03 21:40:01 +0530

    auto push

--- java/java-normal/src/DavidBanas/Constructors.java
+++ java/java-normal/src/DavidBanas/Constructors.java
@@ -2,7 +2,7 @@ package DavidBanas;
 
 import java.util.Arrays;
 
-public class Constructors<x> {
+public class Constructors{
 
     public int x = 10;
     public int y = 5;
@@ -10,7 +10,9 @@ public class Constructors<x> {
     Constructors(int x , float y){
         int z = x + (int)y;
         System.out.println(x + " + " + y + " = " + z);
+
     }
+
 
     Constructors(String name){
         System.out.println("Hi " +  name + ", I am your constructor");
@@ -20,8 +22,14 @@ public class Constructors<x> {
         System.out.println(Arrays.toString(x));
     }
 
+    // arrays pending
+    Constructors(String[] x ){
+
+    }
+
     public static void primitiveTester(int x){
         x = 50;
+        // here we changed x but its value is valid in this primitvieTester block only
     }
 
     public static void main(String[] args) {
@@ -32,5 +40,8 @@ public class Constructors<x> {
         Constructors cs = new Constructors("shub");
         Constructors sum = new Constructors(15, 5);
         Constructors arbi = new Constructors(1,2,3,4);
+        // Here we are just creating objects but the constructor is running with that.
+        // Constructors are mostly used to declare stuff required during program
+        // or run pre-requisites for the rest of the program
     }
 }
