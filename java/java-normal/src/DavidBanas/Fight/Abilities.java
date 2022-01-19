d654c6af213bf474aa6f4b53754549c802e8588d
shubmehetre <ghostfr3k@protonmail.com>
2022-01-19 21:40:02 +0530

    auto push

---
+++ java/java-normal/src/DavidBanas/Fight/Abilities.java
@@ -0,0 +1,7 @@
+package DavidBanas.Fight;
+
+import java.util.Random;
+
+public class Abilities extends Battle{
+    Random r = new Random();
+}
--- java/java-normal/src/DavidBanas/Fight/WarriorGame.java
+++ java/java-normal/src/DavidBanas/Fight/WarriorGame.java
@@ -5,7 +5,7 @@ public class WarriorGame {
         Warrior subzero = new Warrior(
                 "Subzero", 1200, 380, 450);
         Warrior scorp = new CriticalHitter(
-                "Scorpian", 1000, 450, 400, .25);
+                "Scorpian", 1000, 450, 400, .15);
 
         Battle.startFight(subzero,scorp);
     }
