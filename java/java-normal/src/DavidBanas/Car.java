e7ebb3b6fa9e03502b255df9db00168ceb9db570
shubmehetre <ghostfr3k@protonmail.com>
2022-01-21 21:40:01 +0530

    auto push

--- java/java-normal/src/DavidBanas/Car.java
+++ java/java-normal/src/DavidBanas/Car.java
@@ -1,11 +1,13 @@
 package DavidBanas;
 
-import DavidBanas.Fight.Battle;
-
-public interface Car {
+public abstract  interface Car {
     void gas();
     void brake();
     void clutch();
+    void bike();
+    default void peddle(){
+        System.out.println("in interface");
+    }
 }
 
 class bmw extends Tester implements Car {
@@ -24,11 +26,20 @@ class bmw extends Tester implements Car {
         System.out.println("Shifting gear");
     }
 
+    @Override
+    public void bike() {
+        System.out.println("from bike");
+    }
+
+
     public static void main(String[] args) {
         Car c1 = new bmw();
         c1.gas();
         c1.clutch();
         c1.brake();
+        c1.bike();
+
+        c1.peddle();
 
         Tester t1 = new Tester();
         System.out.println(t1.getName());
