dd50711687c8c771a8b1c833ef7db042796209e3
shubmehetre <ghostfr3k@protonmail.com>
2022-01-26 21:40:01 +0530

    auto push

---
+++ java/java-normal/src/DavidBanas/Arrays.java
@@ -0,0 +1,22 @@
+package DavidBanas;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+
+        int[] idNums; // declaration
+        idNums = new int[10]; // initialization
+
+        System.out.println("Enter 10 id nums: ");
+        for (int i = 0 ; i < 10 ; i++){
+            idNums[i] = sc.nextInt();
+        }
+        System.out.println("Id nums entered are: ");
+        for (int i = 0 ; i < 10 ; i++){
+            System.out.print(idNums[i] + " ");
+        }
+
+    }
+}
