c72784861f1cdbe2dfd61790fe852920f8387ba7
shubmehetre <ghostfr3k@protonmail.com>
2022-06-23 21:40:01 +0530

    auto push

---
+++ python/hackerrank/if_else.py
@@ -0,0 +1,15 @@
+#!/bin/python
+if __name__ == '__main__':
+    def raw_input() :
+        return input()
+
+    n = int(raw_input().strip())
+
+    if (n % 2 != 0 ):
+        print("Weird")
+    elif (n in range(2,5)):
+        print("Not Weird")
+    elif (n in range(6,21)):
+        print("Weird")
+    elif (n > 20):
+        print("Not Weird")
