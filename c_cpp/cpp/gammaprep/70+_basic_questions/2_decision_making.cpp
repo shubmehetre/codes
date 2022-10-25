9b5dae3fe037db2ee0debe36ff4e9cd7bf88aab4
shubmehetre <ghostfr3k@protonmail.com>
2022-10-25 21:40:01 +0530

    auto push

---
+++ c_cpp/cpp/gammaprep/70+_basic_questions/2_decision_making.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ *
+ * 4! = 4 x 3 x 2 x 1
+*/
+}
---
+++ c_cpp/cpp/switchcase.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+enum Cars {Bugatti, Audi, Bently, Rolls_Royce};
+
+int main(){
+
+  Cars car = Bently;
+
+  // cout << typeid(Cars).name();
+
+  char x = 'x';
+
+  switch(x){
+
+    case 'x': cout << "this is x"; break;
+
+  }
+  cout << endl;
+
+  /* switch (car) {
+    case Audi: cout << "this is a Audi"; break;
+    case Bugatti: cout << "this is a Bugatti"; break;
+    case Bently: cout << "this is a Bently"; break;
+    case Rolls_Royce: cout << "this is a Rolls_Royce"; break;
+  } */
+
+}
