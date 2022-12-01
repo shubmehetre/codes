428adb404f6fa326de03f32fe966ba989322da9f
shubmehetre <ghostfr3k@protonmail.com>
2022-12-01 21:40:01 +0530

    auto push

---
+++ c_cpp/cpp/hashing.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+
+int main(){
+  unordered_set<int> set1;
+
+  set1.insert(1);
+  set1.insert(1);
+  set1.insert(2);
+
+  cout << *set1.find(1) << endl;
+  cout << *set1.find(2);
+}
