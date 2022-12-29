3f6ca66e9ddd5112d1b8842b969cda38741457f5
shubmehetre <ghostfr3k@protonmail.com>
2022-12-29 21:40:01 +0530

    auto push

--- c_cpp/cpp/vectors.cpp
+++ c_cpp/cpp/vectors.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <vector>
 
 using namespace std;
 
@@ -10,7 +9,7 @@ int main() {
   for (int i = 0; i < v.size(); i++) {
 
     for (int j = 0; j < v[0].size(); j++) {
-      cout << v[i][j] << " ";
+      cout << v[i][j] << " | ";
     }
 
     cout << endl;
